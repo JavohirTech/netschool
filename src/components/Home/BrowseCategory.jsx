7d36ee7e81233f5b07fe43aa389520f6f5d041c0
Javohir G'ulomjonov <javohirtech@gmail.com>
2022-08-09 22:59:13 +0500

    browse homepage from home

---
+++ src/components/Home/BrowseCategory.jsx
@@ -0,0 +1,49 @@
+import '../../styles_umar.css'
+import { Link } from 'react-router-dom';
+
+const BrowseCategory = ()=>{
+    return(
+        <>
+            <div className="browse_category">
+                <small className="home_theme">
+                    Browse Category
+                </small>
+                <h2>Popular Topics To Learn</h2>
+                <div className="browse_category_cards">
+                    <Link to="/architect">
+                        <div className="browse_category_card">
+                            <i className='fas fa-cog fa-3x'></i>
+                            <h2>Architect Design</h2>
+                            <small>3 courses</small>
+                        </div>
+                    </Link>
+                    <Link to="/business">
+                        <div className="browse_category_card">
+                            <i className='fas fa-computer fa-3x'></i>
+                            <h2>Business Management</h2>
+                            <small>3 courses</small>
+                        </div>
+                    </Link>
+                    <Link to="/manage">
+                        <div className="browse_category_card">
+                            <i className='fas fa-server fa-3x'></i>
+                            <h2>Business Management</h2>
+                            <small>3 courses</small>
+                        </div>
+                    </Link>
+                    <Link to="/ai">
+                        <div className="browse_category_card">
+                        <i className="fas fa-microchip fa-3x"></i>
+                            <h2>Artificial Inteligence</h2>
+                            <small>3 courses</small>
+                        </div>                  
+                    </Link>
+                    
+                </div>
+                <Link className='main_buttoncha browse_btn' to="/courses">See All Categories</Link>
+            </div>
+        </>
+    )
+}
+
+export default BrowseCategory
